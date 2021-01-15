673207f05b3aa267fdf1a7d029cc0823a25d0770
Andy Jian <jia.jian@mail.mcgill.ca>
2021-01-14 21:36:59 -0800

    lifted pokemon state up from PokemonsGrid

--- src/components/pokemonsGrid.tsx
+++ src/components/pokemonsGrid.tsx
@@ -1,39 +1,20 @@
-import React, { useState, useEffect } from "react";
-import { Button, Grid } from "@material-ui/core";
+import React, { useState } from "react";
+import { Grid } from "@material-ui/core";
 
-import { Pokemon, PokemonType } from "../models/Pokemon";
+import { Pokemon } from "../models/Pokemon";
 import { PokemonDisplay } from "./pokemonDisplay";
-import { getPokemons } from "../services/pokeServices/pokemonService";
 import Pagination from "./common/pagination";
 
-export const PokemonsGrid: React.FunctionComponent<any> = (props) => {
-  let dummyPokemons: Pokemon[] = [];
-  for (let i = 1; i <= 9; i++) {
-    let currPokemon: Pokemon = {
-      name: "bulbasaur",
-      id: i,
-      weight: 20,
-      picture:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
-      type: [PokemonType.Grass, PokemonType.Poison],
-    };
-    dummyPokemons.push(currPokemon);
-  }
-
-  const [currPokemons, setCurrPokemons] = useState<Pokemon[]>(dummyPokemons);
-
+interface IPokemonsGrid {
+  pokemons: Pokemon[];
+}
+export const PokemonsGrid: React.FunctionComponent<IPokemonsGrid> = ({
+  pokemons,
+}) => {
   const [pageSize, setPageSize] = useState<number>(9);
-  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [currPage, setCurrPage] = useState<number>(1);
 
-  useEffect(() => {
-    const getPokemon = async () => {
-      let pokemons = await getPokemons((currentPage - 1) * pageSize, pageSize);
-      setCurrPokemons(pokemons);
-    };
-    getPokemon();
-  }, [currentPage, pageSize]);
-
-  let pokemonDisplays = currPokemons.map((pokemon) => {
+  let pokemonDisplays = pokemons.map((pokemon) => {
     return (
       <Grid key={pokemon.id} xs={4} item>
         <PokemonDisplay pokemon={pokemon} />
@@ -47,10 +28,10 @@ export const PokemonsGrid: React.FunctionComponent<any> = (props) => {
         {pokemonDisplays}
       </Grid>
       <Pagination
-        itemsCount={currPokemons.length}
+        itemsCount={pokemons.length}
         pageSize={pageSize}
-        currentPage={currentPage}
-        onPageChange={setCurrentPage}
+        currentPage={currPage}
+        onPageChange={setCurrPage}
       />
     </>
   );
