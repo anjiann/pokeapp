578442c24b3d431db8c0739d3f5fa0e1b566727d
Andy Jian <jia.jian@mail.mcgill.ca>
2021-01-14 21:57:23 -0800

    rewrite Pokedex component as a functional component

--- src/components/pokedex.tsx
+++ src/components/pokedex.tsx
@@ -1,82 +1,93 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "./searchbar";
 import PokemonsGrid from "./pokemonsGrid";
 import SideBar from "./sideBar";
 import ListGroup from "./common/listGroup";
 
+import { getPokemons } from "../services/pokeServices/pokemonService";
 import { getGenerations } from "../services/pokeServices/generationService";
 import { getTypes } from "../services/pokeServices/typeService";
+import { Pokemon } from "../models/Pokemon";
 
-class Pokedex extends React.Component<any, any> {
-  state = {
-    categories: [{ name: "generation" }, { name: "type" }],
-    filters: [],
-    generations: [],
-    versions: [],
-    types: [],
-    currentPage: 1,
-    pageSize: 4,
-    searchQuery: "",
-    selectedCategory: null,
-    selectedFilter: null,
-    sortColumn: { path: "title", order: "asc" },
-  };
+const Pokedex: React.FunctionComponent<any> = () => {
+  const [categories, setCategories] = useState<any[]>([
+    { name: "generation" },
+    { name: "type" },
+  ]);
+  const [filters, setFilters] = useState<any[]>([]);
+  const [generations, setGenerations] = useState<any[]>([]);
+  const [types, setTypes] = useState<any[]>([]);
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
-  async componentDidMount() {
-    let data = await getGenerations();
-    const generations = [{ _id: "", name: "All Generations" }, ...data];
-    data = await getTypes();
-    const types = [{ _id: "", name: "All Types" }, ...data];
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [pageSize, setPageSize] = useState<number>(9);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [selectedCategory, setSelectedCategory] = useState<any>(null);
+  const [selectedFilter, setSelectedFilter] = useState<any>(null);
 
-    this.setState({ generations });
-    this.setState({ filters: generations });
-    this.setState({ types });
-  }
+  useEffect(() => {
+    const initializeData = async () => {
+      let data = await getGenerations();
+      const generations = [{ _id: "", name: "All Generations" }, ...data];
 
-  handleCategorySelect = (category: any) => {
-    this.setState({ selectedCategory: category });
+      data = await getTypes();
+      const types = [{ _id: "", name: "All Types" }, ...data];
+
+      let pokemons = await getPokemons();
+      pokemons = pokemons.slice(0, 9); //temp test
+      setGenerations(generations);
+      setTypes(types);
+
+      setFilters(generations);
+      setPokemons(pokemons);
+    };
+    initializeData();
+  }, []);
+
+  const handleCategorySelect = (category: any) => {
+    setSelectedCategory(category);
     switch (category.name) {
       case "generation":
-        this.setState({ filters: this.state.generations });
+        setFilters(generations);
         break;
       case "type":
-        this.setState({ filters: this.state.types });
+        setFilters(types);
         break;
     }
   };
 
-  handleFilterSelect = (filter: string) => {
-    this.setState({ selectedFilter: filter, searchQuery: "", currentPage: 1 });
+  const handleFilterSelect = (filter: string) => {
+    setSelectedFilter(filter);
+    setSearchQuery("");
+    setCurrentPage(1);
   };
 
-  render() {
-    return (
-      <div className="row" style={{ margin: 0 }}>
-        <div className="col-2">
-          <SideBar />
-        </div>
-        <div className="col-2">
-          <ListGroup
-            items={this.state.categories}
-            selectedItem={this.state.selectedCategory}
-            onItemSelect={this.handleCategorySelect}
-            textProperty="name"
-            valueProperty="name"
-            isHorizontal={true}
-          />
-          <ListGroup
-            items={this.state.filters}
-            selectedItem={this.state.selectedFilter}
-            onItemSelect={this.handleFilterSelect}
-          />
-        </div>
-        <div className="col">
-          <SearchBar />
-          <PokemonsGrid />
-        </div>
+  return (
+    <div className="row" style={{ margin: 0 }}>
+      <div className="col-2">
+        <SideBar />
       </div>
-    );
-  }
-}
+      <div className="col-2">
+        <ListGroup
+          items={categories}
+          selectedItem={selectedCategory}
+          onItemSelect={handleCategorySelect}
+          textProperty="name"
+          valueProperty="name"
+          isHorizontal={true}
+        />
+        <ListGroup
+          items={filters}
+          selectedItem={selectedFilter}
+          onItemSelect={handleFilterSelect}
+        />
+      </div>
+      <div className="col">
+        <SearchBar />
+        <PokemonsGrid pokemons={pokemons} />
+      </div>
+    </div>
+  );
+};
 
 export default Pokedex;
