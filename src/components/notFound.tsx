2850b2bd7910287e4bf38a78f9a861e69a2614a1
muhtasimchowdhury <muhtasim.chowdhury@uoit.net>
2021-01-17 17:51:10 -0500

    fit not-found image to screen

--- src/components/notFound.tsx
+++ src/components/notFound.tsx
@@ -4,6 +4,7 @@ export const NotFound: React.FunctionComponent<any> = () => {
       <img
         src={process.env.PUBLIC_URL + "/page404_LI.jpg"}
         alt="Page not found"
+        style={{width:"100%", height:"50vw"}}
       />
     </div>
   );
